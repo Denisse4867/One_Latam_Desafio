7f62c130ec69e8016fc6cf6fa067c6491e1ae840
Denisse4867 <melissapamela777@gmail.com>
2025-09-23 22:20:22 -0300

    Se agrega cambios de limpieza y evitar duplicados para mejorar la consistencia de datos

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 let amigos = [];
 
 //agregamos los amigoss
-function agregarAmigos() {
+function agregarAmigo() {
     const input = document.getElementById("amigo");
     const nombre = input.ariaValueMax.trim();
 
@@ -47,5 +47,60 @@ function sortearAmigo() {
 
     const resultado = document.getElementById("resultado");
     resultado.innerHTML = `<li>El amigo secreto es: <strong>${elegido}</strong></li>`;
+}
+
+
+//agregar entrega
+document.addEventListener("DOMContentLoaded", () => {
+    const input = document.getElementById("amigo");
+    input.addEventListener("keydown" , (e) => {
+        if (e.key === "Enter") agregarAmigo();
+    })
+})
+
+
+//limpieza de resultados, tipo 'truncate'
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+    alert("Por favor, inserte un nombre.");
+
+    return;
+}
+
+
+amigos.push(nombre);
+actualizarLista();
+input.value = "";
+
+document.getElementById("resultado").innerHTML = "";
+}
+
+
+
+function agregarAmigo () {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert ("Por favor, inserte un nombre.");
+        return;
+    }
+
+
+
+const existe = amigos.some (a => a.toLowerCase() === nombre.toLowerCase());
+if (existe) {
+    alert("Ese nombre ya existe en esta lista");
+    return;
+}
+
+amigos.push(nombre);
+actualizarLista();
+input.value = "";
+document.getElementById("resultado").innerHTML = "";
+
 }
 
